50f79f17679380538485cb988a8d359303059349
sunnydegraaf <sunny.de.graaf@twopine.nl>
2022-04-11 14:30:42 +0200

    Update Cube.js

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { motion, useViewportScroll, useTransform } from 'framer-motion'
+import { useEffect, useState } from 'react'
 
 export default function Cube({ cube, index }) {
+    const [pageHeight, setPageHeight] = useState()
+
+    useEffect(() => {
+        setPageHeight(document.documentElement.scrollHeight)
+    }, [])
 
     const { color, width, height, scrollFactor } = cube
     const { scrollY } = useViewportScroll();
     const left = Math.floor(Math.random() * -20)
     const right = Math.floor(Math.random() * -20)
     const top = Math.floor(Math.random() * 100)
-    const y1 = useTransform(scrollY, [0, document.documentElement.scrollHeight], [0, scrollFactor]);
-    const y2 = useTransform(scrollY, [0, document.documentElement.scrollHeight], [0, scrollFactor]);
-
+    const y = useTransform(scrollY, [0, pageHeight], [0, scrollFactor]);
+ 
     return (
         <>
-            <motion.span style={{ background: color, width: width, height: height, left: `${left}%`, top: `${top}%`, y: y1 }} initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1.5, transition: {duration: .2}}} viewport={{ once: true }} className='cube' ></motion.span>
-            <motion.span style={{ background: color, width: width, height: height, right: `${right}%`, top: `${top}%`, y: y2 }} initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1.5,  transition: {duration: .2}}} viewport={{ once: true }} className='cube'></motion.span>
+            <motion.span style={{ background: color, width: width, height: height, left: `${left}%`, top: `${top}%`, y: y }} initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1.5, transition: {duration: .2}}} viewport={{ once: true }} className='cube' ></motion.span>
+            <motion.span style={{ background: color, width: width, height: height, right: `${right}%`, top: `${top}%`, y: y }} initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1.5,  transition: {duration: .2}}} viewport={{ once: true }} className='cube'></motion.span>
         </>
     )
-}
+} 
