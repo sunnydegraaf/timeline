fc4474b1039a4516dba4aa7d92e5f37bfd0f61ab
sunnydegraaf <sunny.de.graaf@twopine.nl>
2022-04-11 16:18:59 +0200

    Added future mode

--- src/components/Container.js
+++ src/components/Container.js
@@ -9,31 +9,31 @@ export default function Container() {
 
     let cubes = [
         {
-          width: 9,
-          height: 9,
-          color: '#e83e8c',
-          scrollFactor: -2000
+            width: 9,
+            height: 9,
+            color: '#e83e8c',
+            scrollFactor: -2000
         },
         {
-          width: 3,
-          height: 3,
-          color: '#fd7e14',
-          scrollFactor: -1100
+            width: 3,
+            height: 3,
+            color: '#fd7e14',
+            scrollFactor: -1100
         },
         {
-          width: 6,
-          height: 6,
-          color: '#007bff',
-          scrollFactor: -1500
+            width: 6,
+            height: 6,
+            color: '#007bff',
+            scrollFactor: -1500
         }
-      ]
+    ]
 
     return (
         <>
             {containers.map((container, index) => {
                 if (index % 2 === 0) {
                     // Renders odd container on odd index
-                    return <>
+                    return <section className={`${container.future ? "future" : ""}`}>
                         <div className='odd' key={index}>
                             {cubes.map((cube, index) => {
                                 return <Cube cube={cube} index={index} />
@@ -42,11 +42,11 @@ export default function Container() {
                             <Content title={container.title} body={container.body} />
                         </div>
                         <Seperator />
-                    </>
+                    </section>
                 } else {
                     // Renders even container on odd index
-                    return <>
-                        <div className='even'>
+                    return <section className={`${container.future ? "future" : ""}`}>
+                        <div className='even' key={index}>
                             {cubes.map((cube, index) => {
                                 return <Cube cube={cube} index={index} />
                             })}
@@ -54,7 +54,7 @@ export default function Container() {
                             <ImageContainer img={container.img} year={container.year} />
                         </div>
                         <Seperator reverse={'reverse'} />
-                    </>
+                    </section>
                 }
             })}
         </>
