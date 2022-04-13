d54bef757f59c9393f3d4af2ac8f410bf901cbfd
sunnydegraaf <sunny.de.graaf@twopine.nl>
2022-04-13 11:31:28 +0200

    Added share links

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,25 @@ import BrinkLogo from '../img/logo.svg'
 
 export default function Header() {
 
-  console.log('link is: ', window.location.href)
+  let link = window.location.href
 
   const copyLink = () => {
-    let link = window.location.href
+    
     navigator.clipboard.writeText(link)
     console.log('copied')
   }
 
   return (
     <div className='header'>
-        <a id='go-to'>Naar website</a>
+        <a href='https://www.brink.nl/' id='go-to'>Naar website</a>
         <img className='logo' src={BrinkLogo} alt={'Brink logo'}/>
         <h1>50 jaar een kleurrijke reis door de gebouwde omgeving</h1>
         <ul className='share'>
             <li>Deel</li>
-            <li><a href='https://twitter.com/brink_nl'>Twitter</a></li>
-            <li><a href='https://www.instagram.com/brink.nl/'>Instagram</a></li>    
-            <li><a href='https://www.linkedin.com/company/brink-nl/'>LinkedIn</a></li>
-            <li><a onClick={copyLink}>Kopieer link</a></li>
+            <li><a href={`https://twitter.com/share?url=${link}&via=brink_nl`}>Twitter</a></li>
+            <li><a href={`https://www.facebook.com/sharer/sharer.php?u=${link}`}>Facebook</a></li>    
+            <li><a href={`https://www.linkedin.com/sharing/share-offsite/?url=${link}`}>LinkedIn</a></li>
+            <li><a href='#' onClick={copyLink}>Kopieer link</a></li>
         </ul>
     </div>
   )
